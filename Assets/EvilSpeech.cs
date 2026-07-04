b86671500a614a29b90d4064650e7f7a65abf28c
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-04 14:19:44 +0200

    Added Evil variables

--- Assets/EvilSpeech.cs
+++ Assets/EvilSpeech.cs
@@ -1,11 +1,19 @@
+using System;
 using UnityEngine;
 
 public class EvilSpeech : MonoBehaviour
 {
+    public Vector3 velocity;
+    public int mainCooldown;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         print("*evil laugh* x9 I'm evil");
     }
 
+    void Update()
+    {
+    }
 }
